3b0ab06548b3b6c5efb24537bd562b3c2392001b
Manuela Larissa Stivanin <210542@upf.br>
2026-06-14 23:16:18 -0300

    Adding new functions

--- graphroute.cpp
+++ graphroute.cpp
@@ -9,8 +9,28 @@ class Graphroute{
 private:
     struct node{
         T value;
+        int grauEntrada = 0; //dentro do nó ou fora na classe como um unordered map???
         std::unordered_set<node*> links;
     };
+    std::unordered_map<T, node> graph;
 
+    node* find(const T& val){
+        auto it = graph.find(val);
+        if (it == graph.end()) { //se chagar aqui quer dizer que não achou, por isso retorna nulo
+            return nullptr;
+        }
+        return &it->second; //se encontrou retorna o nó
+    }
+
+public:
+
+    void insert_node(const T& val){
+        if (graph.count(val) != 0)
+            return;
+        node aux;
+        aux.value = val;
+        graph[val] = aux;
+    }
+    
 };
 }
